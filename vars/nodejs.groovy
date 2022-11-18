14fb6ad749fd5ba56ed596919d8cdb6872d78141
Msahu201989 <Msahu201989@gmail.com>
2022-11-18 21:39:05 +0530

    var

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -1,9 +1,12 @@
 def call() {
   node {
-
+    try {
         common.codeQuality()
         common.codeChecks()
         common.artifacts()
+    }  catch (Exception e) {
+        mail bcc: '', body: "Build Failure ${RUN_TESTS_DISPLAY_URL}", cc: '', from: 'mukeshsahu20@gmail.com', replyTo: '', subject: 'BUILD FAILURE', to: 'mukeshsahu20@gmail.com'
+    }
 
   }
 
