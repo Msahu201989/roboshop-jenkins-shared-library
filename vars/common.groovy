e6d63d9087f20c81272c39d5b84df3d4faf12d8a
Msahu201989 <Msahu201989@gmail.com>
2022-11-20 19:22:48 +0530

    var

--- vars/common.groovy
+++ vars/common.groovy
@@ -66,7 +66,7 @@ def artifacts() {
         stage('Publish Artifacts') {
             withCredentials([usernamePassword(credentialsId: 'NEXUS', passwordVariable: 'nexusPass', usernameVariable: 'nexusUser')]) {
                 sh '''
-          curl -v -u ${nexususer}:${nexuspass} --upload-file ${COMPONENT}-${TAG_NAME}.zip http://nexus.roboshop.internal:8081/repository/${COMPONENT}/${COMPONENT}-${TAG_NAME}.zip
+          curl -v -u ${nexusUser}:${nexusPass} --upload-file ${COMPONENT}-${TAG_NAME}.zip http://nexus.roboshop.internal:8081/repository/${COMPONENT}/${COMPONENT}-${TAG_NAME}.zip
             '''
             }
         }
