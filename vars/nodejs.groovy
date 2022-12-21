08d8e0fe1b7426beba1ccb1c042dbbf9cd4f6e61
Mukesh <msahu201989@gmail.com>
2022-12-21 20:10:14 +0530

    Docker

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -5,7 +5,8 @@ def call() {
         common.codeCheckout()
         common.codeQuality()
         common.codeChecks()
-        common.artifacts()
+//      common.artifacts()
+        common.docker()
     }  catch (Exception e) {
         mail bcc: '', body: "Build Failure ${RUN_TESTS_DISPLAY_URL}", cc: '', from: 'mukeshsahu20@gmail.com', replyTo: '', subject: 'BUILD FAILURE', to: 'mukeshsahu20@gmail.com'
     }
