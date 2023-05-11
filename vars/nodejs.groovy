7cc858cbd197854390c72f362de8d166138422eb
Mukesh <Stunty@007>
2023-05-11 18:48:44 +0530

    learning devops

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -1,16 +1,84 @@
 def call() {
-  env.APPTYPE = "nodejs"
-  node {
-    try {
-        common.codeCheckout()
-        common.codeQuality()
-        common.codeChecks()
-        common.artifacts()
-    }  catch (Exception e) {
-        mail bcc: '', body: "Build Failure ${RUN_TESTS_DISPLAY_URL}", cc: '', from: 'mukeshsahu20@gmail.com', replyTo: '', subject: 'BUILD FAILURE', to: 'mukeshsahu20@gmail.com'
+    pipeline {
+
+        agent any
+
+        stages {
+
+            stage('code quality') {
+                steps {
+                    echo 'code quality'
+                }
+            }
+
+            stage('Style checks') {
+                steps {
+                    echo 'style checks'
+                }
+            }
+
+            stage('unit Tests') {
+                steps {
+                    echo 'Unit Test'
+                }
+            }
+
+            stage('Download dependencies') {
+                when { tag "*" }
+                steps {
+                    echo 'Download Dependencies'
+                }
+            }
+
+            stage('Prepare Artifact') {
+                when { tag "*" }
+                steps {
+                    echo 'Prepare Artifact'
+                }
+            }
+
+            stage('Publish Artifact') {
+                when { tag "1.0.0" }
+                steps {
+                    echo 'Publish Artifact'
+                }
+            }
+        }
     }
-
-  }
-
 }
 
+
+
+
+
+
+
+
+
+// @Library('roboshop') _
+//
+// env.COMPONENT="cart"
+// nodejs()
+
+
+
+
+
+
+
+//def call() {
+//  env.APPTYPE = "nodejs"
+//  node {
+//    try {
+//        common.codeCheckout()
+//        common.codeQuality()
+//        common.codeChecks()
+//        common.artifacts()
+//    }  catch (Exception e) {
+//        mail bcc: '', body: "Build Failure ${RUN_TESTS_DISPLAY_URL}", cc: '', from: 'mukeshsahu20@gmail.com', replyTo: '', subject: 'BUILD FAILURE', to: 'mukeshsahu20@gmail.com'
+//    }
+//
+//  }
+//
+//}
+//
