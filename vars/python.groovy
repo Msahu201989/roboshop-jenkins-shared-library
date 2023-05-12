c82d77dc04814acc21fef7c4ca8348ad1d5d8948
Mukesh <Stunty@007>
2023-05-12 11:30:19 +0530

    learning devops

--- vars/python.groovy
+++ vars/python.groovy
@@ -1,46 +1,15 @@
 def call() {
-    pipeline {
-
-        agent any
-
-        stages {
-
+    node {
+        try {
+            common.codeCheckout()
             common.codeQuality()
+            common.codeChecks()
+            common.artifacts()
+        }  catch (Exception e) {
+            mail bcc: '', body: "Build Failure ${RUN_TESTS_DISPLAY_URL}", cc: '', from: 'mukeshsahu20@gmail.com', replyTo: '', subject: 'BUILD FAILURE', to: 'mukeshsahu20@gmail.com' }
 
-            stage('Style checks') {
-                steps {
-                    echo 'style checks'
-                }
-            }
-
-            stage('unit Tests') {
-                steps {
-                    echo 'Unit Test'
-                }
-            }
-
-            stage('Download dependencies') {
-                when { tag "*" }
-                steps {
-                    echo 'Download Dependencies'
-                }
-            }
-
-            stage('Prepare Artifact') {
-                when { tag "*" }
-                steps {
-                    echo 'Prepare Artifact'
-                }
-            }
-
-            stage('Publish Artifact') {
-                when { tag "1.0.0" }
-                steps {
-                    echo 'Publish Artifact'
-                }
-            }
-        }
     }
+
 }
 
 
