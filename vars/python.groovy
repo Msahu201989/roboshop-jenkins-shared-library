38f5c438b1fb396b48f42ed0240485f2a328ea56
Mukesh <Stunty@007>
2023-05-12 11:45:57 +0530

    learning devops

--- vars/python.groovy
+++ vars/python.groovy
@@ -1,4 +1,51 @@
+//def call() {
+//    pipeline {
+//
+//        agent any
+//
+//        stages {
+//
+//            common.codeQuality()
+//
+//            stage('Style checks') {
+//                steps {
+//                    echo 'style checks'
+//                }
+//            }
+//
+//            stage('unit Tests') {
+//                steps {
+//                    echo 'Unit Test'
+//                }
+//            }
+//
+//            stage('Download dependencies') {
+//                when { tag "*" }
+//                steps {
+//                    echo 'Download Dependencies'
+//                }
+//            }
+//
+//            stage('Prepare Artifact') {
+//                when { tag "*" }
+//                steps {
+//                    echo 'Prepare Artifact'
+//                }
+//            }
+//
+//            stage('Publish Artifact') {
+//                when { tag "1.0.0" }
+//                steps {
+//                    echo 'Publish Artifact'
+//                }
+//            }
+//        }
+//    }
+//}
+
+
 def call() {
+    env.APPTYPE = "python"
     node {
         try {
             common.codeCheckout()
@@ -6,26 +53,10 @@ def call() {
             common.codeChecks()
             common.artifacts()
         }  catch (Exception e) {
-            mail bcc: '', body: "Build Failure ${RUN_TESTS_DISPLAY_URL}", cc: '', from: 'mukeshsahu20@gmail.com', replyTo: '', subject: 'BUILD FAILURE', to: 'mukeshsahu20@gmail.com' }
+            mail bcc: '', body: "Build Failure ${RUN_TESTS_DISPLAY_URL}", cc: '', from: 'mukeshsahu20@gmail.com', replyTo: '', subject: 'BUILD FAILURE', to: 'mukeshsahu20@gmail.com'
+        }
 
     }
 
 }
 
-
-//def call() {
-//    env.APPTYPE = "python"
-//    node {
-//        try {
-//            common.codeCheckout()
-//            common.codeQuality()
-//            common.codeChecks()
-//            common.artifacts()
-//        }  catch (Exception e) {
-//            mail bcc: '', body: "Build Failure ${RUN_TESTS_DISPLAY_URL}", cc: '', from: 'mukeshsahu20@gmail.com', replyTo: '', subject: 'BUILD FAILURE', to: 'mukeshsahu20@gmail.com'
-//        }
-//
-//    }
-//
-//}
-//
