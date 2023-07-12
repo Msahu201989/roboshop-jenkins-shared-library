0525af7425d18685f1e0aa946a67ee7087b3763f
Mukesh <Stunty@007>
2023-07-12 15:39:37 +0530

    learning devops

--- vars/common.groovy
+++ vars/common.groovy
@@ -71,8 +71,8 @@ def artifacts() {
     }
 
 //
-/
-//def codeCheckout() {
+
+///def codeCheckout() {
 // stage('code checkout') {
 //   sh 'find . | sed 1d |xargs rm -rf'
 //   git branch: 'main', url: "https://github.com/Msahu201989/${COMPONENT}.git"
