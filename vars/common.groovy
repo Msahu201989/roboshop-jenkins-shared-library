1113778cb21aeb7e86bc6a358ceea4801e133f1d
Mukesh <Stunty@007>
2023-08-03 21:51:40 +0530

    July 23 learning devops

--- vars/common.groovy
+++ vars/common.groovy
@@ -25,7 +25,7 @@ def codeChecks() {
     }
 }
 def artifacts() {
-    if ( env.TAG_NAME ==~ ".*" ) {
+//    if ( env.TAG_NAME ==~ ".*" ) {
         stage('Prepare Artifacts') {
             if (env.APPTYPE == "nodejs") {
                 sh '''
@@ -64,7 +64,7 @@ def artifacts() {
 
 
 def docker() {
-    if ( env.TAG_NAME ==~ ".*" ) {
+//    if ( env.TAG_NAME ==~ ".*" ) {
         //if ( env.TAG_NAME ==~ ".*" ) {
 
         stage('Build Docker Image') {
